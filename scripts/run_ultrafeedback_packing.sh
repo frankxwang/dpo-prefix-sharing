3745644fae77e8091e09c26ceab46825efadbbf5
Franklin Wang <fxwang@mit.edu>
2024-10-29 17:00:45 -0400

    Update run_ultrafeedback_packing.sh

--- scripts/run_ultrafeedback_packing.sh
+++ scripts/run_ultrafeedback_packing.sh
@@ -6,7 +6,7 @@ packing_length=2048
 batch_size=4
 output_dir="ultrafeedback_packing"
 
-accelerate launch --config_file 'configs/zero3.yaml'  train_dpo.py \
+accelerate launch --config_file 'configs/zero3.yaml' train_dpo.py \
   --dataset_name=$dataset \
   --model_name_or_path=$model \
   --per_device_train_batch_size 1 \
